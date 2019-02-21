84c0f2e73bae252b6cd3bc2e0fc1d1a8d222cb43
Alexander Popov <alex.popov.tech@gmail.com>
2019-02-21 14:00:30 +0200

    add ts

--- specs/spec.ts
+++ specs/spec.ts
@@ -1,9 +1,10 @@
-const {Test, Suite, BeforeEach} = require('toundra');
+const {Test, Suite, BeforeEach, AfterEach} = require('toundra');
 const {Browser, have, be} = require('selenidejs');
 const {Builder, Capabilities} = require('selenium-webdriver');
 
 BeforeEach(async () => {
   Browser.configuration.webdriver = await new Builder().withCapabilities(Capabilities.chrome()).build();
+  Browser.configuration.timeout = 2000;
 });
 AfterEach(async () => {
   await Browser.quit();
